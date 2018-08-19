5916bf3a7ef5c0924a39f95c6c0e12662cbf945f
GadgetCopter <rjdobrz@gmail.com>
2018-08-18 17:47:25 -0700

    comments and finalized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,20 +13,23 @@ var word;
 document.getElementById("wins").textContent = "Wins: " + wins;
 document.getElementById("wrong-guess").textContent = "Incorrect guesses so far: ";
 
+// Starts gmae and created random cat wor from wordList
 var gameStart = function() {
     letters = [];
     word = wordList[Math.floor(Math.random() * wordList.length)];
-    for (var i = 0; i < word.length; i++) {
+    for (var i = 0; i < word.length; i++) { 
         letters[i] = "_"
     }
     document.getElementById("word-key").textContent = letters.join(" ");
     console.log(word);
 }
 
+// listens for user input to start game & guessing
 document.onkeydown = function(event) {
     userGuess = event.key;
     var correctGuess = false;
 
+    // Compares user intput to each letter in chosen word
    if (acceptedInput.includes(userGuess)){
         for (var i = 0; i < word.length; i++){
             if (userGuess === word[i]) {
@@ -35,6 +38,8 @@ document.onkeydown = function(event) {
                 correctGuess = true;
             }
         }
+
+        // resets game and checks if the game is won
         if (!letters.includes("_")){
             guessesLeft = 13;
             incorrectList = [];
@@ -45,14 +50,18 @@ document.onkeydown = function(event) {
             alert("Purr-fect! the word was " + word);
             gameStart();
         }
+        
+        
         if (correctGuess) return;
         
+        // checks to see if guess was wrong and records/prints incorrect guess on screen
         if (incorrectList.indexOf(userGuess) < 0) {
             incorrectList.push(userGuess);
             document.getElementById("wrong-guess").textContent = "Incorrect guesses so far: " + incorrectList.join(" ");
             guessesLeft--
             document.getElementById("guesses-left").textContent = "Guesses remaining: " + guessesLeft;
             
+            // ends game if there are no more lives left
             if (guessesLeft <= 0){
                 alert("Ouch! Sorry, the word was " + word);
                 guessesLeft = 13;
